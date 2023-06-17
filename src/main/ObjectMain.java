53b63f0ce8be66099d0f5c807060d2db4a061eba
Bilov Yuriy <jurib@rambler.ru>
2023-06-18 00:23:18 +0300

    Initiated 3

--- src/main/ObjectMain.java
+++ src/main/ObjectMain.java
@@ -1,11 +1,14 @@
 package main;
 
 import main.data.Card;
+import main.data.MasterCard;
 
 public class ObjectMain {
     public static void main(String[] args) {
 
-        Card masterCard = new Card();
+        Card masterCard = new MasterCard();
+
+        masterCard.
 
     }
 }
